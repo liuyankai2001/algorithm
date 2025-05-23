bd58f6e5e8609de6496e58c724939ec4c266781c
liuyankai <liuyankai23@mails.ucas.ac.cn>
2025-05-23 16:42:43 +0800

    冒泡、选择、插入、希尔、快速排序算法编写完毕

--- 2.基础算法/1.排序算法.py
+++ 2.基础算法/1.排序算法.py
@@ -16,28 +16,76 @@ class Sort:
 
     # 冒泡排序
     def bubble_sort(self):
-        pass
+        arr = self.arr
+        for i in range(self.arr_num - 1, 0, -1):
+            for j in range(0, i):
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
     # 选择排序
     def select_sort(self):
-        pass
+        arr = self.arr
+        for i in range(0, self.arr_num - 1):
+            minpos = i
+            for j in range(i, self.arr_num):
+                if arr[minpos] > arr[j]:
+                    minpos = j
+            arr[minpos], arr[i] = arr[i], arr[minpos]
 
     # 插入排序
     def insert_sort(self):
-        pass
+        arr = self.arr
+        for i in range(1, self.arr_num):
+            num = arr[i]
+            j = i
+            while j > 0:
+                if arr[j - 1] > num:
+                    arr[j] = arr[j - 1]
+                else:
+                    break
+                j = j - 1
+            arr[j] = num
 
     # 希尔排序
     def shell_sort(self):
-        pass
+        arr = self.arr
+        gap = self.arr_num >> 1
+        while gap > 0:
+            for i in range(gap, self.arr_num):
+                num = arr[i]
+                j = i
+                while j > 0:
+                    if arr[j - gap] > num:
+                        arr[j] = arr[j - gap]
+                    else:
+                        break
+                    j = j - gap
+                arr[j] = num
+            gap = gap >> 1
 
     # 快速排序
     def partition(self, left, right):
-        pass
-
+        arr = self.arr
+        k = left
+        for i in range(left,right):
+            if arr[i]<arr[right]:
+                arr[k],arr[i] = arr[i],arr[k]
+                k+=1
+        arr[k],arr[right] = arr[right],arr[k]
+        return k
     def quick_sort(self, left, right):
-        pass
+        if left < right:
+            pivot = self.partition(left, right)
+            self.quick_sort(left, pivot - 1)
+            self.quick_sort(pivot + 1, right)
 
 
 if __name__ == '__main__':
     s = Sort(10, 100)
+    print(s.arr)
+    # s.bubble_sort()
+    # s.select_sort()
+    # s.insert_sort()
+    # s.shell_sort()
+    # s.quick_sort(0,9)
     print(s.arr)
